82011e6238451c7bb5c3eeecb6b094af98544f3f
Joseph <indiekaj@gmail.com>
2024-07-25 21:49:04 +0300

    installing tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline bg-slate-700">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

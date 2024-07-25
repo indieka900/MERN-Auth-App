cbd89d93cd25320ffa52c8a7783ed64add1ad0f1
Joseph <indiekaj@gmail.com>
2024-07-26 00:45:55 +0300

    formik and toster packeges

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import profile from "../assets/profile.png";
 import styles from "../styles/username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from 'formik';
+import { usernameValidate } from "../helper/validate";
 
 export default function Username() {
+    const formik = useFormik({
+        initialValues :{ username: ''},
+        validate: usernameValidate,
+        validateOnBlur: false,
+        validateOnChange: false,
+        onSubmit: async values => {
+            console.log(values);
+        }
+    })
+
   return (
     <div className="container mx-auto">
+        <Toaster position="top-center" reverseOrder={false} />
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
             <div className="title flex flex-col items-center">
@@ -13,7 +27,7 @@ export default function Username() {
                 Explore More by connecting with us
                 </span>
             </div>
-            <form className="py-1">
+            <form className="py-1" onSubmit={formik.handleSubmit}>
                 <div className="profile flex justify-center py-4">
                 <img
                     src={profile}
@@ -25,9 +39,8 @@ export default function Username() {
                 <div className="textbox flex flex-col items-center gap-6">
                 <input
                     className={styles.textbox}
+                    {...formik.getFieldProps('username')}
                     type="text"
-                    name=""
-                    id=""
                     placeholder="Username"
                 />
                 <button className={styles.btn} type="submit">

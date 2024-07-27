4525c66fa7ccb6fcdbdb73ccf83a97cc20f66dd9
Joseph <indiekaj@gmail.com>
2024-07-27 22:13:11 +0300

    other components

--- client/src/components/Recovery.jsx
+++ client/src/components/Recovery.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import styles from "../styles/username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { passwordValidate } from "../helper/validate";
 
 export default function Recovery() {
+  const formik = useFormik({
+    initialValues: { password: "" },
+    validate: passwordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
-    <div>Recovery</div>
-  )
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-4xl font-bold">Recovery</h4>
+            <span className="py-2 text-xl w-2/3 text-center text-gray-500">
+              Enter OTP to recover password
+            </span>
+          </div>
+          <form className="pt-1" onSubmit={formik.handleSubmit}>
+            <div className="textbox flex flex-col items-center gap-6">
+              <div className="input text-center">
+                <span className="py-4 text-sm text-left text-gray-500">
+                  Please Enter 6-digit OTP sent to your email
+                </span>
+                <input
+                  className={styles.textbox}
+                  type="number"
+                  placeholder="OTP"
+                />
+              </div>
+
+              <button className={styles.btn} type="submit">
+                Recover
+              </button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Can&apos;t get OTP? 
+                <button className="text-red-500" >
+                  Resent
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/helper/validate.jsx
+++ client/src/helper/validate.jsx
@@ -5,6 +5,11 @@ export async function usernameValidate (values) {
     return errors;
 }
 
+export async function passwordValidate (values) {
+    const errors = passwordVerify({},values);
+    return errors;
+}
+
 function usernameVerify(error={}, values) {
     if(!values.username){
         error.username = toast.error("Username required...!");
@@ -13,4 +18,31 @@ function usernameVerify(error={}, values) {
     }
 
     return error;
+}
+
+function passwordVerify(errors = {}, values){
+
+    const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
+
+export async function resetPasswordValidation(values){
+    const errors = passwordVerify({},values);
+
+    if(values.password !== values.confirm_password){
+        errors.exist = toast.error("Password not match...!");
+    }
+
+    return errors;
 }
